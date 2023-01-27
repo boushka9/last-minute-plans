3e6ab62056315681ce412abfa57d789ecd39d3a7
Samantha Gosselin <samanthag2009@gmail.com>
2023-01-26 19:18:04 -0600

    Fixed dropdown menus in HTML and added triggers in script.js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+
+//Initialize Cuisine Dropdown
+var instance = M.Dropdown.getInstance("#dropdown1");
+
+$('.dropdown-trigger').dropdown();
+
+//Initialize Dietary Restrictions Dropdown
+var instance = M.Dropdown.getInstance("#dropdown2");
+
+$('.dropdown-trigger').dropdown();
+
+//Initialize Movie Genre Dropdown
+var instance = M.Dropdown.getInstance("#dropdown3");
+
+$('.dropdown-trigger').dropdown();
