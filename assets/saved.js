a22862988d1aa97eda217c763317b62c1e8d11a6
Danny Boushka <boushka9@gmail.com>
2023-01-28 16:39:40 -0700

    added styling to pages to determine layout for cards in saved plans for local storage

--- assets/saved.js
+++ assets/saved.js
@@ -1,26 +1,26 @@
-$(document).ready(function () { 
+// $(document).ready(function () { 
 
 
- //THIS FUNCTION TO RESULTS.JS PAGE, WHEN RESULTS ARE RENDERED, BUTTON CAN BE CLICKED    
-    // On click, if item is not included in
-    $("#favorite-btn").on("click", (event) => {
-        event.preventDefault();
-        // If city name is not in cityHistory, add it to localStorage with key favorite to retrieve value
-        if (favoriteHistory.includes(itemName) === false) {
-            favoriteHistory.push(itemName);
-            localStorage.setItem("favorite", JSON.stringify(favoriteHistory));
-        }
-    })
+//  //THIS FUNCTION TO RESULTS.JS PAGE, WHEN RESULTS ARE RENDERED, BUTTON CAN BE CLICKED    
+//     // On click, if item is not included in
+//     $("#favorite-btn").on("click", (event) => {
+//         event.preventDefault();
+//         // If city name is not in cityHistory, add it to localStorage with key favorite to retrieve value
+//         if (favoriteHistory.includes(itemName) === false) {
+//             favoriteHistory.push(itemName);
+//             localStorage.setItem("favorite", JSON.stringify(favoriteHistory));
+//         }
+//     })
 
-    // on page load, for each savedItem i of favoriteHistory, run func to render
-    for (var i = 0; i < favoriteHistory.length; i++) {
-        renderFavorites(favoriteHistory[i]);
-    }
+//     // on page load, for each savedItem i of favoriteHistory, run func to render
+//     for (var i = 0; i < favoriteHistory.length; i++) {
+//         renderFavorites(favoriteHistory[i]);
+//     }
 
-    function renderFavorites(favCards) {
-        let favItem = $("<div>").addClass("fav-item").text(favCards);
-        $("saved-container").append(favItem);
-    }
+//     function renderFavorites(favCards) {
+//         let favItem = $("<div>").addClass("fav-item").text(favCards);
+//         $("saved-container").append(favItem);
+//     }
 
     
 
@@ -36,4 +36,4 @@ $(document).ready(function () {
 
 
 
-});
+// });
